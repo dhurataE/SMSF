65ff2bc785dcdd4f36e38848bfc1403ac3edbecc
Dhurate_H <32544334+dhurataE@users.noreply.github.com>
2025-01-12 00:13:57 +0100

    Add files via upload

---
+++ Klasat/csObjekti.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace SMS.Klasat
+{
+
+    public class csObjekti
+    {
+
+        protected SqlConnection Connection;
+        
+        public csObjekti()
+        {
+            string connection = ConfigurationManager.ConnectionStrings["ConnectionStringB"].ToString();
+            Connection = new SqlConnection(connection);
+        }
+        public void fnConnection()
+        {
+            string connA = ConfigurationManager.ConnectionStrings["ConnectionStringB"].ToString();
+            string connB = ConfigurationManager.ConnectionStrings["ConnectionStringA"].ToString();
+
+        }
+         public int RunPr(string storePRName, IDataParameter[] parameters)
+        {
+            int res = 0;
+            try
+            {
+                Connection.Open();
+
+                SqlCommand command = new SqlCommand(storePRName, Connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                foreach (SqlParameter parameter in parameters)
+                {
+                    command.Parameters.Add(parameter);
+
+                }
+                command.ExecuteNonQuery();
+
+                Connection.Close();
+            }
+            catch (Exception) {
+              //  string error = "Gabim gjate thirrjes se Procedurave...!";
+               
+            }
+            return res;
+        }
+
+        public DataSet RunQuery( string query, string tableName )
+        {
+            DataSet ds = new DataSet();
+
+            Connection.Open();
+            SqlDataAdapter sqlda = new SqlDataAdapter();
+
+            SqlCommand cmd = new SqlCommand(query, Connection);
+            cmd.CommandType = CommandType.Text;
+            sqlda.SelectCommand = cmd;
+            sqlda.Fill(ds, tableName);
+            Connection.Close();
+            return ds;
+        
+        }
+
+
+
+    }
+}
---
+++ Klasat/csPacienti.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace SMS.Klasat
+{
+    public class csPacienti
+    {
+        public static int RuajPacientin(int Id_Pacienti, string Emri_P, string Mbiemri_P, string DataLindjes_P, string Numri_Kontaktues, string Historia_P)
+        {
+            int resultati = 0;
+
+
+            csObjekti obj = new csObjekti();
+            SqlParameter[] parameter = new SqlParameter[]
+                {
+             new SqlParameter("@Case", 1),
+             new SqlParameter("@id_Pacienti", Id_Pacienti),
+new SqlParameter("@Emri",Emri_P),
+new SqlParameter("@Mbiemri", Mbiemri_P),
+new SqlParameter("@DataLindjes", DataLindjes_P),
+new SqlParameter("@Nr_Kontaktues", Numri_Kontaktues),
+new SqlParameter("@Historia_Pacientit", Historia_P)
+        };
+            resultati = obj.RunPr("[prPacienti]", parameter);
+            return resultati;
+        }
+
+
+
+        public static int ModifikoPacientin(int Id_Pacienti, string Emri_P, string Mbiemri_P, string DataLindjes_P, string Numri_Kontaktues, string Historia_P)
+        {
+            int rezulati = 0;
+            csObjekti objO = new csObjekti();
+            SqlParameter[] parameter = new SqlParameter[]
+                {
+new SqlParameter("@Case", 2),
+new SqlParameter("@id_Pacienti", Id_Pacienti),
+new SqlParameter("@Emri",Emri_P),
+new SqlParameter("@Mbiemri", Mbiemri_P),
+new SqlParameter("@DataLindjes", DataLindjes_P),
+new SqlParameter("@Nr_Kontaktues", Numri_Kontaktues),
+new SqlParameter("@Historia_Pacientit", Historia_P) };
+            rezulati = objO.RunPr("prPacienti", parameter);
+
+            return rezulati;
+        }
+        public static int FshiPacientin(int Id_Pacienti)
+        {
+            int rezulati = 0;
+            csObjekti objO = new csObjekti();
+            SqlParameter[] parameter = new SqlParameter[]
+                {
+                    new SqlParameter("@Case",3),
+                    new SqlParameter("@id_Pacienti", Id_Pacienti)
+                };
+            return rezulati;
+        }
+    }
+}
